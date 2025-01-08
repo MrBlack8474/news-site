6247190cac4e2608a0c39fb9ddd7d849d2a94b7e
MrBlack8474 <121707146+MrBlack8474@users.noreply.github.com>
2025-01-08 22:40:36 +0530

    Updated

--- admin/save-post.php
+++ admin/save-post.php
@@ -1,45 +1,45 @@
 <?php
-include "config.php";
-// session_start();
-if(isset($_FILES['fileToUpload'])){
-    $error=array();
+   include'config.php';
+  if(isset($_FILES['fileToUpload'])){
+    $errors= array();
+   //  $file_n=&$file_name;
     $file_name=$_FILES['fileToUpload']['name'];
     $file_size=$_FILES['fileToUpload']['size'];
     $file_tmp=$_FILES['fileToUpload']['tmp_name'];
     $file_type=$_FILES['fileToUpload']['type'];
-    $x=explode('.',$_FILES['name']);
-    $file_ext=strtolower(end($x));
+    $file_ext =(pathinfo($file_name, PATHINFO_EXTENSION));
     $extension=array("jpeg","jpg","png");
-    if(in_array($file_ext,$extension)=== false){
-        $error[]="this file format is not support try only valid format";
-
-    }
-    if($file_size>2097152){
-        $error="File size must be 2 MB or lower";
-    }
-    if(empty($error)==true){
-        move_uploaded_file($file_tmp,"admin/upload/".$file_name);
-    }else{
-        print_r($error);
-    }
+     if(in_array($file_ext,$extension)===false){
+        $errors[]="this extension file is not allowed, please choose a JPG PNG FILE";
+     }else{
+     if($file_size>2097152){
+        $errors[]= "file must be less then 2 MB";
+     }
+     if(empty($errors)==true){
+        move_uploaded_file($file_tmp,"upload/".$file_name);
+     }else{
+        print_r($errors);
+        die();
+     }
 }
+  }
 session_start();
-$title=mysqli_real_escape_string($conn,$_POST['post_title']);
-$description=mysqli_real_escape_string($conn,$_POST['postdesc']);
-$category= mysqli_real_escape_string($conn,$_POST['category']);
-$date =date("d M Y");
 
-$author=$_SESSION['user_id'];
+    $title=mysqli_real_escape_string($conn,$_POST['post_title']);
+    $descriptio=mysqli_real_escape_string($conn, $_POST['postdesc']);
+    $category=mysqli_real_escape_string($conn, $_POST['category']);
+    
+   //  $image=mysqli_real_escape_string($conn, $_POST['fileToUpload']);
+    $date=date("d F,Y") ;
+    $author=$_SESSION['user_id'];
+    $sql="INSERT INTO post (title,description,category,post_img,author,post_date)  
+    VALUES ('{$title}','{$descriptio}','{$category}','{$file_name}',{$author},'{$date}');";
+    $sql.="UPDATE category SET post = post+1 WHERE category_name='{$category}' ";
+    if(mysqli_multi_query($conn,$sql)){
 
-$sql="INSERT INTO post(title,description,category,post_date,author,post_img)
-        VALUE ('{$title}','{$description}','{$category}','{$date}','{$author}','{$file_name}');";
-$sql.="UPDATE category SET post=post+1 WHERE Category_id ={$category}";
-
-if(mysqli_multi_query($conn,$sql)){
-    header("{$hostname}/admin/post.php");
-}else{
-    echo "<div class='alert-danger'>Query Failed.>/div>";
-}
-
-
-?>
+    header("location: post.php");
+    exit;
+    }else{
+      echo "<div class='alert alert-danger'>Query failed</div>";
+    }
+ 
